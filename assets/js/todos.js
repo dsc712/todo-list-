8895053fbe813fc4a2f8f5600ff2f6d3991807c5
dsc712 <devyendushekhar712@gmail.com>
2018-01-01 01:29:59 +0530

    added files of todol-list project

---
+++ assets/js/todos.js
@@ -0,0 +1,29 @@
+$("ul").on("click","li" ,function(){
+  $(this).toggleClass("completed");
+
+});
+
+ $("ul").on("click","span",function(e){
+ 	$(this).parent().fadeOut(500 , function(){
+ 		$(this).remove() ;
+ 	});
+ 	e.stopPropagation();
+ });
+
+ $("input[type = 'text']").keypress(function(event)
+ {
+
+    if(event.which === 13)
+     {
+       var todo = $(this).val() ;
+
+       $(this).val("") ;
+
+        $("ul").append("<li><span><i class = 'fa fa-trash'></i></span>" + todo + "</li>") ;
+     }
+
+   });
+
+$(".fa-plus").click(function(){
+	 $("input[type = 'text']").fadeToggle(50) ;
+})
